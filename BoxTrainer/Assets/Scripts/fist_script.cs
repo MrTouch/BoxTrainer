b982a481b01030d2cf353370d7ee15522dd34ec4
Marlon <marlonstadler@gmx.ch>
2019-03-28 15:29:34 +0100

    sensei und fist scripts created

---
+++ BoxTrainer/Assets/Scripts/fist_script.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fist_script : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 5.0f;
+
+    [SerializeField]
+    private GameObject leftHand;
+
+    [SerializeField]
+    private GameObject rightHand;
+
+    [SerializeField]
+    private sensei_script sensei;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        setHands();
+
+        if (Input.GetKey("1"))
+        {
+            transform.position = new Vector3(transform.position.x,transform.position.y,transform.position.z - moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey("2"))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + moveSpeed * Time.deltaTime);
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.name == "collider")
+        {
+            sensei.lastCollision = col;
+            sensei.collidingHand = this.gameObject;
+        }
+    }
+
+    void setHands() 
+    {
+        Vector3 leftHandPos = leftHand.transform.position;
+        Vector3 rightHandPos = rightHand.transform.position;
+
+        if (leftHandPos.z < rightHandPos.z)
+        {
+            sensei.frontHand = leftHand;
+            sensei.backHand = rightHand;
+        }
+        else
+        {
+            sensei.frontHand = rightHand;
+            sensei.backHand = leftHand;
+        }
+    }
+}
---
+++ BoxTrainer/Assets/Scripts/sensei_script.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sensei_script : MonoBehaviour
+{
+    [SerializeField]
+    private Move currentMove = Move.jab;
+
+    public Collision lastCollision;
+    public GameObject collidingHand;
+
+    public GameObject frontHand;
+    public GameObject backHand;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (currentMove)
+        {
+            case Move.jab:
+                if (lastCollision != null)
+                {
+                    if (collidingHand == frontHand)
+                    {
+                        Debug.Log("Nice one");
+                    }
+                    else
+                    {
+                        Debug.Log("Much to learn you still have my young padawan.");
+                    }
+                }
+                break;
+            
+            case Move.punch:
+                if (lastCollision != null)
+                {
+                    if (collidingHand == backHand)
+                    {
+                        Debug.Log("Nice one");
+                    }
+                    else
+                    {
+                        Debug.Log("Much to learn you still have my young padawan.");
+                    }
+                }
+                break;
+            
+            case Move.uppercutLeft:
+                //Do stuff
+                break;
+
+            case Move.uppercutRight:
+                //Do stuff
+                break;
+
+            case Move.hookLeft:
+                //Do stuff
+                break;
+
+            case Move.hookRight:
+                //Do stuff
+                break;
+
+            case Move.block:
+                //Do stuff
+                break;
+
+            case Move.evade:
+                //Do stuff
+                break;
+
+            default:
+                Debug.Log("Unknown move");
+                break;
+        }
+        lastCollision = null;
+    }
+
+
+
+    enum Move {jab,punch,uppercutLeft,uppercutRight,hookLeft,hookRight,block,evade}
+}
